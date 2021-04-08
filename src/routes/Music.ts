fd99662294c62f93109ce2d3a4fbd574ac164d4c
Hwanhee Chae <hh@hwan.me>
2021-04-08 20:07:58 +0900

    Add find with title or/and artist

--- src/routes/Music.ts
+++ src/routes/Music.ts
@@ -3,9 +3,42 @@ import dbMusic from '../models/Music';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  dbMusic.find({}).then((musics) => {
-    res.status(200).json(musics);
+router.get('/', (req: express.Request, res) => {
+  const title: string = req.query.title as string;
+  const artist: string = req.query.artist as string;
+  const titleRegex = RegExp(title, 'i');
+  const artistRegex = RegExp(artist, 'i');
+
+  const query = {
+    $and: [],
+  };
+
+  if (title) {
+    query.$and.push({
+      $or: [{
+        'info.title_name': titleRegex,
+      }, {
+        'info.title_yomigana': titleRegex,
+      }, {
+        'info.ascii': titleRegex,
+      }],
+    });
+  }
+
+  if (artist) {
+    query.$and.push({
+      $or: [{
+        'info.artist_name': titleRegex,
+      }, {
+        'info.artist_yomigana': titleRegex,
+      }, {
+        'info.ascii': artistRegex,
+      }],
+    });
+  }
+
+  dbMusic.find(query).then((found) => {
+    res.status(200).json(found);
   });
 });
 
