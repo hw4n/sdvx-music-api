2a6321e2f9fd128c07427d25a5187ad0412e8e8d
Hwanhee Chae <hh@hwan.me>
2021-04-08 20:17:31 +0900

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import yargs from 'yargs';
+import cors from 'cors';
 
 import initialize from './utils/initialize';
 import musicRoute from './routes/Music';
@@ -12,6 +13,7 @@ const { argv } = yargs(process.argv);
 
 initialize(argv);
 
+app.use(cors());
 app.use('/v1', musicRoute);
 
 const PORT = process.env.PORT || 3333;
